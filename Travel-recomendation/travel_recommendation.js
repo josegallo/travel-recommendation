33b77289dd9ad0a041ac621cee6577f144b93d9e
Jose Gallo <josegallo@gmail.com>
2024-11-09 10:37:28 +0100

    Upload files

---
+++ Travel-recomendation/travel_recommendation.js
@@ -0,0 +1,9 @@
+// Function for the Search button
+function search() {
+    alert("Searching..."); // Placeholder for actual search function
+}
+
+// Function for the Clear button
+function reset() {
+    document.querySelector('.search-bar input').value = '';
+}
