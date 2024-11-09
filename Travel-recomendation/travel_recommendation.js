f4a15d4f16c65b55b8e669f1e35bb2fe681e6844
Jose Gallo <josegallo@gmail.com>
2024-11-09 13:40:05 -0500

    contact us page

--- Travel-recomendation/travel_recommendation.js
+++ Travel-recomendation/travel_recommendation.js
@@ -7,3 +7,22 @@ function search() {
 function reset() {
     document.querySelector('.search-bar input').value = '';
 }
+
+// Function for alert when submiting Contact Form 
+    function submitForm() {
+
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const message = document.getElementById("message").value.trim();
+            // Check if all fields are filled
+        if (name && email && message) {
+             alert("Thank you " + name + ", for your message. We will respond to you soon.\nKind Regards\nBa Da Boom Team");
+
+            return true; // Allow form submission
+        } else {
+            alert("Please fill in all fields.");
+            return false; // Prevent form submission
+        }
+    }
+
+    
